e7b67d6d08db77104e79115414493a4b3a98332f
yohimik <yohimik@gmail.com>
2025-05-22 14:10:19 +0400

    feature: init

---
+++ public/head.js
@@ -0,0 +1,13 @@
+import * as fflate from 'https://cdn.skypack.dev/fflate@0.8.2?min';
+var Module = typeof Module != 'undefined' ? Module : {};
+Module.dynamicLibraries = [
+	"filesystem_stdio",
+	// "ref_gl.so",
+	"ref_soft.so",
+	"menu",
+	"server.wasm",
+	"client.wasm",
+]
+Module.arguments = ['-windowed']
+Module['INITIAL_MEMORY'] = 16777216 * 8
+Module['canvas'] = document.getElementById('canvas')
